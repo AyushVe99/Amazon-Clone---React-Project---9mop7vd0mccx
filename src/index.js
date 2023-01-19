d5da8a385d351bb6566bdad8025b122c4959e88e
AyushVe99 <Ayushnishu407@gmail.com>
2023-01-19 14:30:09 +0530

    new

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { StateProvider } from "./components/StateProvider";
+import reducer,{ initialState} from "./components/reducer";
 
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+<StateProvider initialState={initialState} reducer={reducer}>
+<App />
+</StateProvider>
+, document.getElementById("root"));
